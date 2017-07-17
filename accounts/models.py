ba2fd4cafd5f2c87be4771a52a68ced099cdc178
Ricardo Meireles <cadomeireles@gmail.com>
2017-07-17 13:55:27 -0300

    Create custom user model

---
+++ accounts/models.py
@@ -0,0 +1,66 @@
+import uuid
+
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model
+    """
+
+    # Fields
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        primary_key=True,
+    )
+    name = models.CharField(
+        _('name'),
+        max_length=120,
+    )
+    email = models.EmailField(
+        _('email'),
+        unique=True,
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=False,
+        help_text=_(
+            'Designates whether this user should be treated as active.'
+        ),
+    )
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_(
+            'Designates whether the user can log into this admin site.'
+        ),
+    )
+
+    # Override manager
+    objects = UserManager()
+
+    # Auth settings
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self):
+        """
+        Return name
+        """
+        return self.name
+
+    def get_short_name(self):
+        """
+        Split name and return first result
+        """
+        return self.name.split()[0]
