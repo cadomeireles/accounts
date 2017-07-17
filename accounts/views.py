320b5cf9ae3e066a4123d25b973a7429d603b632
Ricardo Meireles <cadomeireles@gmail.com>
2017-07-17 16:03:14 -0300

    Create and send an email to user email confirmation

--- accounts/views.py
+++ accounts/views.py
@@ -11,3 +11,16 @@ class SignupView(CreateView):
     form_class = SignupForm
     success_url = r('accounts:signup')
     template_name = 'accounts/signup.html'
+
+    def form_valid(self, form):
+        """
+        Save user and send email confirmation
+        """
+        # Save user
+        user = form.save()
+
+        # Create and send email confirmation
+        email_confirmation = user.email_confirmations.create()
+        email_confirmation.send()
+
+        return super().form_valid(form)
