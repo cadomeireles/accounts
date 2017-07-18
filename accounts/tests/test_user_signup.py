5416d13a34ba2ffd968b39bd46762c1013917001
Ricardo Meireles <cadomeireles@gmail.com>
2017-07-18 09:51:30 -0300

    Create a module for email confirmation tests

--- accounts/tests/test_user_signup.py
+++ accounts/tests/test_user_signup.py
@@ -1,7 +1,6 @@
 import pytest
-from django.core import mail
 
-from ..models import User, EmailConfirmation
+from ..models import User
 
 
 @pytest.mark.django_db
@@ -49,25 +48,3 @@ class TestUserSignup:
 
         # Check status code
         assert resp.status_code == status_code
-
-    def test_send_email_confirmation(self, client, signup_data):
-        """
-        An email with token to confirmation must be sent
-        """
-
-        # Make the request
-        client.post('/accounts/signup/', signup_data)
-
-        # Ensure the email confirmation was created and sent
-        assert EmailConfirmation.objects.exists()
-        assert len(mail.outbox) == 1
-
-        email = mail.outbox[0]
-        email_confirmation = EmailConfirmation.objects.first()
-
-        # Ensure the token was generated
-        assert email_confirmation.token
-
-        # Check email infos
-        assert email_confirmation.user.email in email.to
-        assert email_confirmation.token in email.body
